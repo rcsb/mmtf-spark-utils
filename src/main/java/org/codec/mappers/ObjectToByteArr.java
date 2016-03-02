09b6977fc564965799a3cd9d45e964d256073742
abradle <anthony.richard.bradley@gmail.com>
2016-03-01 16:28:25 -0800

    Change type

--- src/main/java/org/codec/mappers/ObjectToByteArr.java
+++ src/main/java/org/codec/mappers/ObjectToByteArr.java
@@ -2,11 +2,11 @@ package org.codec.mappers;
 
 import org.apache.spark.api.java.function.PairFunction;
 import org.codec.biojavaencoder.EncoderUtils;
-import org.codec.dataholders.CalphaDistBean;
+import org.codec.dataholders.CalphaAlignBean;
 
 import scala.Tuple2;
 
-public class ObjectToByteArr implements PairFunction<Tuple2<String, CalphaDistBean>, String, byte[]>{
+public class ObjectToByteArr implements PairFunction<Tuple2<String, CalphaAlignBean>, String, byte[]>{
 
 	/**
 	 * 
@@ -14,7 +14,7 @@ public class ObjectToByteArr implements PairFunction<Tuple2<String, CalphaDistBe
 	private static final long serialVersionUID = 8225004637753153167L;
 
 	@Override
-	public Tuple2<String, byte[]> call(Tuple2<String, CalphaDistBean> t) throws Exception {
+	public Tuple2<String, byte[]> call(Tuple2<String, CalphaAlignBean> t) throws Exception {
 		EncoderUtils eu = new EncoderUtils();
 		return new Tuple2<String, byte[]>(t._1,eu.getMessagePack(t._2));
 	}
