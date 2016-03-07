799aeb4b3f8816a51626def9a4261c71435f775a
abradle <anthony.richard.bradley@gmail.com>
2016-03-07 10:38:01 -0800

    Fix

--- src/main/java/org/rcsb/mmtf/mappers/GetSequenceId.java
+++ src/main/java/org/rcsb/mmtf/mappers/GetSequenceId.java
@@ -61,8 +61,8 @@ public class GetSequenceId implements PairFunction<Tuple2<Integer,Integer>,Strin
 		if (ps2.getLength() > length) {
 			length = ps2.getLength();
 		}
-		System.out.println(ps1);
-		System.out.println(pair.getNumIdenticals());
+		System.out.println(ps1.toString()+" vs "+ps2.toString()+" -> "+pair.getNumSimilars()+" and "+pair.getNumIdenticals());
+		
 		return new Tuple2<String, Float>(data.get(inputTuple._1)._1+"__"+data.get(inputTuple._2)._1, (float) (pair.getNumIdenticals()/(double)length));
 	}
 
