18981b04869ad84ca9bcb010183edf836e9c2358
abradle <anthony.richard.bradley@gmail.com>
2016-03-07 10:55:06 -0800

    Fix

--- src/main/java/org/rcsb/mmtf/mappers/GetSequenceId.java
+++ src/main/java/org/rcsb/mmtf/mappers/GetSequenceId.java
@@ -49,7 +49,7 @@ public class GetSequenceId implements PairFunction<Tuple2<Integer,Integer>,Strin
 		ProteinSequence ps1 = new ProteinSequence(data.get(inputTuple._1)._2.getSequence());
 		ProteinSequence ps2 = new ProteinSequence(data.get(inputTuple._2)._2.getSequence());
 
-		PairwiseSequenceAligner<ProteinSequence, AminoAcidCompound> smithWaterman = Alignments.getPairwiseAligner(ps1, ps2, PairwiseSequenceAlignerType.LOCAL, penalty, matrix);
+		PairwiseSequenceAligner<ProteinSequence, AminoAcidCompound> smithWaterman = Alignments.getPairwiseAligner(ps1, ps2, PairwiseSequenceAlignerType.GLOBAL, new SimpleGapPenalty(), matrix);
 		SequencePair<ProteinSequence, AminoAcidCompound> pair = null;
 		try {
 			pair = smithWaterman.getPair();
@@ -64,9 +64,8 @@ public class GetSequenceId implements PairFunction<Tuple2<Integer,Integer>,Strin
 		if (ps2.getLength() > length) {
 			length = ps2.getLength();
 		}
-		if(pair.getNumSimilars()>0){
-			System.out.println(pair.getAlignedSequences());
-			System.out.println(ps1.toString()+" vs "+ps2.toString()+" -> "+pair.getNumSimilars()+" and "+pair.getNumIdenticals());
+		if(smithWaterman.getSimilarity()>0.0){
+			System.out.println(smithWaterman.getSimilarity());
 		}
 
 		return new Tuple2<String, Float>(data.get(inputTuple._1)._1+"__"+data.get(inputTuple._2)._1, (float) (pair.getNumIdenticals()/(double)length));
