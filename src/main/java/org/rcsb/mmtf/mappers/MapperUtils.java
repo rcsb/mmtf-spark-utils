516545db0d4c86ad642374d1cd48e904737809ab
Anthony Bradley <abradley@anthonys-air-2.dynamic.ucsd.edu>
2016-03-11 11:50:58 -0800

    Updated mapper for reading PDB -> RDD of String/Bytes

--- src/main/java/org/rcsb/mmtf/mappers/MapperUtils.java
+++ src/main/java/org/rcsb/mmtf/mappers/MapperUtils.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.biojava.nbio.structure.Structure;
 import org.biojava.nbio.structure.StructureImpl;
 import org.rcsb.mmtf.biojavaencoder.EncoderUtils;
@@ -78,5 +82,20 @@ public class MapperUtils {
 		// Add the calpha
 		outList.add(new Tuple2<String, byte[]>(pdbCode+"_calpha", calphaBytes));
 		return outList;
+	}
+	
+	/**
+	 * PDB RDD gnerateor. Converts a list of pdb ids to a writeable RDD
+	 * @param sparkContext
+	 * @return
+	 */
+	public JavaPairRDD<Text, BytesWritable> generateRDD(JavaSparkContext sparkContext, List<String> inputList) {
+		// Set up Biojava appropriateyl
+		EncoderUtils encoderUtils = new EncoderUtils();
+		encoderUtils.setUpBioJava();
+		return sparkContext.parallelize(inputList)
+		.mapToPair(new UpdateToCBSMapper())
+		.flatMapToPair(new CBSToBytes())
+		.mapToPair(new StringByteToTextByteWriter());
 	}
 }
